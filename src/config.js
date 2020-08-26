178664401c882018d385988e12e4bf479b2f58e1
SeyiAriyo <58642769+SeyiAriyo@users.noreply.github.com>
2020-08-26 14:49:44 -0400

    small spelling mistakes

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/noteful2',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/noteful',
   API_TOKEN: process.env.API_TOKEN
 };
 

b583d65d3e1b359c0267cb8720de19381fb22aaf
SeyiAriyo <58642769+SeyiAriyo@users.noreply.github.com>
2020-08-26 14:33:42 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/noteful2',
-  API_TOKEN: process.env.API_TOKEN
+  API_TOKEN: process.env.API_TOKE
 };
 
 //password: 
